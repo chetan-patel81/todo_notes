f7a4007de3ffdefdbbdc331bd4ffff1ba9519f81
chetan-patel81 <156655021+chetan-patel81@users.noreply.github.com>
2024-02-12 07:22:02 +0530

    Update style_utils.dart

--- utils/style_utils.dart
+++ utils/style_utils.dart
@@ -4,11 +4,11 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class MyAppStyle {
-  static const String fontFamily = "metropolis";
+  
 
   static TextStyle headStyle(color) {
     return TextStyle(
-      fontFamily: fontFamily,
+      
       fontSize: 34.0,
       fontWeight: FontWeight.bold, color: color,
       // color: MyAppTheme.darkColor,
@@ -17,7 +17,7 @@ class MyAppStyle {
 
   static TextStyle hintStyle(color) {
     return TextStyle(
-      fontFamily: fontFamily,
+  
       fontSize: 11.0,
       fontWeight: FontWeight.normal,
       // color: MyAppTheme.greyColor,
@@ -27,7 +27,7 @@ class MyAppStyle {
 
   static TextStyle titleStyle(color) {
     return TextStyle(
-      fontFamily: fontFamily,
+      
       fontSize: 14.0,
       fontWeight: FontWeight.w500, color: color,
       // color: MyAppTheme.darkColor,
@@ -36,39 +36,12 @@ class MyAppStyle {
 
   static TextStyle btnStyle(color) {
     return TextStyle(
-      fontFamily: fontFamily,
+
       fontSize: 18.0,
       fontWeight: FontWeight.bold,
       color: color,
       // color: MyAppTheme.lightColor,
     );
   }
-  static TextStyle nameStyle(color) {
-    return TextStyle(
-      fontFamily: fontFamily,
-      fontSize: 16.0,
-      fontWeight: FontWeight.bold,
-      color: color,
-      // color: MyAppTheme.lightColor,
-    );
-  }
-
-
-
-  static TextStyle bigShadowStyle(color) {
-    return TextStyle(
-      fontFamily: fontFamily,
-      fontSize: 48.0,
-      fontWeight: FontWeight.bold,
-      color: color,
-      shadows: const [
-        Shadow(
-          color: Colors.black,
-          offset: Offset(4, 08),
-          blurRadius: 4,
-        ),
-      ],
-      // color: MyAppTheme.lightColor,
-    );
-  }
+  
 }
